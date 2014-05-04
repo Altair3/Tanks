21ada6637881d011f146715de9e5cfccf9b80c2d
Dean M LeBaron <dml66@rowing.cs.byu.edu>
2014-05-03 18:17:10 -0600

    cleaned up

--- bzagents/PFAgent.py
+++ bzagents/PFAgent.py
@@ -41,7 +41,7 @@ class PFAgent(object):
                         self.constants['team']]
 
         self.commands = []
-        i = 0
+        
         for tank in mytanks:
             totalX = 0
             totalY = 0
@@ -52,7 +52,7 @@ class PFAgent(object):
           
             totalX += deltaX
             totalY += deltaY
-            i+=1
+            
             for obstacle in self.obstacles:
                
                 '''deltaX,deltaY = self.fields.getRepulsiveField(tank,obstacle,tank.oldError,time_diff, 50,10)
@@ -63,61 +63,20 @@ class PFAgent(object):
                 deltaX,deltaY = self.fields.getRepulsiveField(tank, enemy, tank.oldError, time_diff, 150, 1)
                 totalX += deltaX
                 totalY += deltaY
-            print "deltaX" , totalX
-            print "deltaY" , totalY
+           
             '''this is calculating the angle between where you currently are and where you are trying to be updates accordingly'''
             ''' The throttle decreases speed as you approach the object of interest, in theory
                 totalX and Y are where you want to be'''
             theta = math.atan2(totalY, totalX)
             theta = theta - tank.angle
-            print "theta" , theta
+           
             command = Command(tank.index,self.throttle*abs(totalY+totalX),theta,False)
             self.commands.append(command)
-        #command = Command(tank.index,)
-        # self.attack_enemies(tank)
+     
             
         '''should we log these results?'''
         results = self.bzrc.do_commands(self.commands)
 
-    def attack_enemies(self, tank):
-        """Find the closest enemy and chase it, shooting as you go."""
-        best_enemy = None
-        best_dist = 2 * float(self.constants['worldsize'])
-        for enemy in self.enemies:
-            if enemy.status != 'alive':
-                
-                continue
-            dist = math.sqrt((enemy.x - tank.x)**2 + (enemy.y - tank.y)**2)
-            if dist < best_dist:
-                best_dist = dist
-                best_enemy = enemy
-        if best_enemy is None:
-            ''' constructor for Command(index,speed,angular,shoot)'''
-            command = Command(tank.index, 0, 0, False)
-            self.commands.append(command)
-        else:
-            self.move_to_position(tank, best_enemy.x, best_enemy.y)
-
-    def move_to_position(self, tank, target_x, target_y):
-        """Set command to move to given coordinates."""
-        target_angle = math.atan2(target_y - tank.y,
-                                  target_x - tank.x)
-        relative_angle = self.normalize_angle(target_angle - tank.angle)
-        command = Command(tank.index, 1, 2 * relative_angle, True)
-        self.commands.append(command)
-        
-
-    def normalize_angle(self, angle):
-        """Make any angle be between +/- pi."""
-        angle -= 2 * math.pi * int (angle / (2 * math.pi))
-        if angle <= -math.pi:
-            angle += 2 * math.pi
-        elif angle > math.pi:
-            angle -= 2 * math.pi
-        return angle
-    
-    
-  
     
 """ A class to perform the potential field and controller calculations
     problem is we need to pass in elapsedtimes and spread and radius, etc.
