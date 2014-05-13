8bb6c3f831d562055af3134e3bcd9b83ae1993a0
Dean M LeBaron <dml66@rowing.cs.byu.edu>
2014-05-13 12:27:30 -0600

    script

--- rungame.sh
+++ rungame.sh
@@ -1,1 +1,7 @@
-./bin/bzrflag --window-size=1450x725 --red-port=35000 --blue-port=35001 --green-port=35002 --purple-port=35003
+#!/bin/bash
+if [ $1 ]; then
+	./bin/bzrflag --window-size=1450x725 --red-port=35000 --blue-port=35001 --green-port=35002 --purple-port=35003 --default-true-positive=$1 --default-true-negative=$2 --occgrid-width=100 --no-report-obstacles
+
+else
+	./bin/bzrflag --window-size=1450x725 --red-port=35000 --blue-port=35001 --green-port=35002 --purple-port=35003
+fi
