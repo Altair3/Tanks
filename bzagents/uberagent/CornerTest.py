d1fddfd8ef558ccf28b849d524ae9a708f0475ce
Christopher Thomas McNeill <cmcneill@pingpong.cs.byu.edu>
2014-06-14 12:55:03 -0600

    obstacle stuff

---
+++ bzagents/uberagent/CornerTest.py
@@ -0,0 +1,34 @@
+from geo import Point, Line
+from OccGrid import OccGrid
+from ObstacleList import ObstacleList
+import time
+
+def main():
+    grid = OccGrid(800,800,.1)
+    
+    for x in range(-200, 201):
+        for y in range(-200,201):
+            grid.set(x, y, .9)
+            
+    obsList = ObstacleList(grid)
+    startTime = time.time()
+    obsList.scanGrid(-300,100, 200)
+    endTime = time.time()
+    
+    print "Time elapsed:", str(endTime-startTime)
+    
+    myList = obsList.getObstaclePoints()
+    
+    print "Corners:"
+    for p in myList:
+        print "(" + str(p.x) + "," + str(p.y) + ")"
+    
+    
+if __name__ == '__main__':
+    main()
+    
+
+    
+    
+            
+    
---
+++ bzagents/uberagent/ObstacleList.py
@@ -0,0 +1,104 @@
+from OccGrid import OccGrid
+from geo import Point,Line
+
+class ObstacleList(object):
+    
+    def __init__(self, occgrid):
+        
+        self.occgrid = occgrid
+        
+        self.yMax = occgrid.yMax
+        self.yMin = self.yMax * -1
+        self.xMax = occgrid.xMax
+        self.xMin = self.xMax * -1
+        
+        self.daList = []
+        self.threshold = .6
+        self.neighborCheckNumber = 1
+        
+    def getObstaclePoints(self):
+        return self.daList
+        
+    def scanGrid(self, startX, startY, length):
+        for x in range(startX, (startX+length+1)):
+            for y in range(startY, (startY+length+1)):
+                #print "Scanning:", "(" + str(x) + "," + str(y) + ")"
+                if self.isCorner(x,y):
+                    self.daList.append(Point(x,y))
+        
+    def isCorner(self, x, y):
+        
+        if self.occgrid.get(x, y) >= self.threshold:
+            
+            up = self.checkUp(x,y)
+            down = self.checkDown(x,y)
+            left = self.checkLeft(x,y)
+            right = self.checkRight(x,y)
+            
+            if (up and left):
+                if ((not down) and (not right)):
+                    return True
+                else:
+                    return False
+            
+            if (up and right):
+                if ((not down) and (not left)):
+                    return True
+                else:
+                    return False
+                    
+            if (down and left):
+                if ((not up) and (not right)):
+                    return True
+                else:
+                    return False
+                    
+            if (down and right):
+                if ((not up) and (not left)):
+                    return True
+                else:
+                    return False
+            
+        return False
+        
+    def checkUp(self, x, y):
+        number = 0
+        for i in range(1, self.neighborCheckNumber+1):
+            if (y + i) <= self.yMax:
+                prob = self.occgrid.get(x, (y+i))
+                
+                if prob < self.threshold:
+                    return False
+                
+        return True
+        
+    def checkDown(self, x, y):
+        for i in range(self.neighborCheckNumber, 0, -1):
+            if (y - i) >= self.yMin:
+                prob = self.occgrid.get(x, (y-i))
+                
+                if prob < self.threshold:
+                    return False
+                
+        return True
+        
+    def checkRight(self, x, y):
+        for i in range(1, self.neighborCheckNumber+1):
+            if (x + i) <= self.xMax:
+                prob = self.occgrid.get((x+i), y)
+                
+                if prob < self.threshold:
+                    return False
+                
+        return True
+        
+    def checkLeft(self, x, y):
+        for i in range(self.neighborCheckNumber, 0, -1):
+            if (x - i) >= self.xMin:
+                prob = self.occgrid.get((x-i), y)
+                if prob < self.threshold:
+                    return False
+                    
+        return True
+                
+            
