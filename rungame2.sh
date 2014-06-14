4acb675cb02caf128a6ee4c2c02c608507ebbdd5
Dean M LeBaron <dml66@pingpong.cs.byu.edu>
2014-06-14 15:29:14 -0600

    the runner

--- rungame2.sh
+++ rungame2.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+if [ $1 ]; then
 
-./bin/bzrflag --window-size=1450x725 --green-port=35000 --red-port=35001 --world=maps/empty.bzw --red-tanks=1 --green-tanks=1 --default-posnoise=5
+./bin/bzrflag --window-size=1450x725 --green-port=35000 --red-port=35001 --world=maps/fourls2p.bzw --default-posnoise=3 --friendly-fire --respawn-time=240 --max-shots=3 --default-tanks=10 --default-true-positive=.97 --default-true-negative=.9 --occgrid-width=100 --no-report-obstacles
+
+else
+
+./bin/bzrflag --window-size=1450x725 --green-port=45000 --red-port=45001 --world=maps/fourls2p.bzw --default-posnoise=3 --friendly-fire --respawn-time=240 --max-shots=3 --default-tanks=10 --default-true-positive=.97 --default-true-negative=.9 --occgrid-width=100 --no-report-obstacles
+fi
